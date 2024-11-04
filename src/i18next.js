a48945386edeb00bf7d701dc6cb1757f7facb4e4
Coktoco <albert.winiarski6@gmail.com>
2024-11-04 10:58:23 +0100

    - Finished the entire main part of the website

--- src/i18next.js
+++ src/i18next.js
@@ -4,6 +4,8 @@ async function initializeI18next() {
       .use(i18nextHttpBackend)
       .use(i18nextBrowserLanguageDetector)
       .init({
+        ns: ['translation'], // Dodaj inne przestrzenie nazw, jeśli są używane
+        defaultNS: 'translation',
         fallbackLng: 'en',
         debug: true,
         interpolation: {
@@ -13,7 +15,7 @@ async function initializeI18next() {
           loadPath: '/locales/{{lng}}/{{ns}}.json'
         }
       });
-
+    
     updateTranslations();
   } catch (error) {
     console.error('Error initializing i18next:', error);
@@ -32,10 +34,21 @@ function updateTranslations() {
   document.getElementById('output_items_plural').innerText = i18next.t('items', { count: 5 });
 
   const amount = 1234.56;
-  const date = new Date().toLocaleDateString();
+  const date = new Date(); // Example date
+  const userLocale = i18next.language || 'en-US'; 
 
-  document.getElementById('output_currency').innerText = i18next.t('currency', { amount: amount.toFixed(2) });
-  document.getElementById('output_date').innerText = i18next.t('date', { date: date });
+  document.getElementById('output_date').innerText = 
+    i18next.t('date', { 
+      date: new Intl.DateTimeFormat(userLocale).format(date) 
+    });
+    
+  document.getElementById('output_currency').innerText = 
+  i18next.t('currency', { 
+    amount: new Intl.NumberFormat(userLocale, { style: 'currency', currency: 'USD' }).format(amount) 
+  });
+
+  const langDetec = i18next.language;
+  document.getElementById('detected_language').innerText = i18next.t('detected_language_message', { lang: langDetec });
 
   ScrollTrigger.refresh()
 };
@@ -51,12 +64,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.getElementById('lang-pl')?.addEventListener('click', () => {
     i18next.changeLanguage('pl').then(() => {
-      updateTranslations();
-    });
-  });
-
-  document.getElementById('lang-de')?.addEventListener('click', () => {
-    i18next.changeLanguage('de').then(() => {
       updateTranslations();
     });
   });
