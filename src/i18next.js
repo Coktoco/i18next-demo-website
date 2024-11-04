0ecbb04ce30d0f134950948518180b6ab89f8169
Coktoco <albert.winiarski6@gmail.com>
2024-11-04 04:10:12 +0100

    - Huge progress: - Added: -- Prism.js - 90% of the project is done, that's probably the most important part

--- src/i18next.js
+++ src/i18next.js
@@ -1,53 +1,67 @@
 async function initializeI18next() {
-    try {
-      await i18next
-        .use(i18nextHttpBackend)
-        .use(i18nextBrowserLanguageDetector)
-        .init({
-          fallbackLng: 'en',
-          debug: true,
-          interpolation: {
-            escapeValue: false
-          },
-          backend: {
-            loadPath: '/locales/{{lng}}/{{ns}}.json'
-          }
-        });
-  
-      updateTranslations();
-    } catch (error) {
-      console.error('Error initializing i18next:', error);
-    }
-}
-  
-  function updateTranslations() {
-    document.querySelectorAll('[data-i18n]').forEach(element => {
-      element.innerText = i18next.t(element.getAttribute('data-i18n'));
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    initializeI18next();
-  
-    document.getElementById('lang-en')?.addEventListener('click', () => {
-      i18next.changeLanguage('en').then(() => {
-        updateTranslations();
+  try {
+    await i18next
+      .use(i18nextHttpBackend)
+      .use(i18nextBrowserLanguageDetector)
+      .init({
+        fallbackLng: 'en',
+        debug: true,
+        interpolation: {
+          escapeValue: false
+        },
+        backend: {
+          loadPath: '/locales/{{lng}}/{{ns}}.json'
+        }
       });
-    });
-  
-    document.getElementById('lang-pl')?.addEventListener('click', () => {
-      i18next.changeLanguage('pl').then(() => {
-        updateTranslations();
-      });
-    });
 
-    document.getElementById('lang-de')?.addEventListener('click', () => {
-        i18next.changeLanguage('de').then(() => {
-          updateTranslations();
-        });
-      });
+    updateTranslations();
+  } catch (error) {
+    console.error('Error initializing i18next:', error);
+  }
+  ScrollTrigger.refresh()
+};
+
+function updateTranslations() {
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+    element.innerText = i18next.t(element.getAttribute('data-i18n'));
+  });
+  document.getElementById('output').innerHTML = i18next.t('greeting'); 
+  document.getElementById('output1').innerText = i18next.t('welcome_user', { name: 'New user' });
+  document.getElementById('output_age').innerText = i18next.t('age_message', { age: 25 });
+  document.getElementById('output_items').innerText = i18next.t('items', { count: 1 });
+  document.getElementById('output_items_plural').innerText = i18next.t('items', { count: 5 });
+
+  const amount = 1234.56;
+  const date = new Date().toLocaleDateString();
+
+  document.getElementById('output_currency').innerText = i18next.t('currency', { amount: amount.toFixed(2) });
+  document.getElementById('output_date').innerText = i18next.t('date', { date: date });
+
+  ScrollTrigger.refresh()
+};
   
-    i18next.on('languageChanged', (lng) => {
+document.addEventListener('DOMContentLoaded', function () {
+  initializeI18next();
+
+  document.getElementById('lang-en')?.addEventListener('click', () => {
+    i18next.changeLanguage('en').then(() => {
       updateTranslations();
     });
   });
+
+  document.getElementById('lang-pl')?.addEventListener('click', () => {
+    i18next.changeLanguage('pl').then(() => {
+      updateTranslations();
+    });
+  });
+
+  document.getElementById('lang-de')?.addEventListener('click', () => {
+    i18next.changeLanguage('de').then(() => {
+      updateTranslations();
+    });
+  });
+
+  i18next.on('languageChanged', (lng) => {
+    updateTranslations();
+  });
+});
--- src/scrolltrigger.js
+++ src/scrolltrigger.js
@@ -11,44 +11,24 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     ScrollTrigger.create({
         trigger: "#content-box",   
-        start: "top 10",             
+        start: "top top",             
         end: "bottom top",            
         pin: "#content-box-placement",       
         pinSpacing: false             
     });
 
-
-    ScrollTrigger.create({
-        trigger: "#sticky-section1",   
-        start: "top 10",             
-        end: "bottom top",            
-        pin: "#scrollButton1",       
-        pinSpacing: false             
+    const sections = ["#sticky-section1", "#sticky-section2", "#sticky-section3", "#sticky-section4"];
+    
+    sections.forEach((section, index) => {
+        ScrollTrigger.create({
+            trigger: section,   
+            start: "top top",             
+            end: "bottom top",            
+            pin: `#scrollButton${index + 1}`,       
+            pinSpacing: false,
+        });
     });
 
-    ScrollTrigger.create({
-        trigger: "#sticky-section2",   
-        start: "top 10",             
-        end: "bottom top",            
-        pin: "#scrollButton2",       
-        pinSpacing: false             
-    });
-
-    ScrollTrigger.create({
-        trigger: "#sticky-section3",   
-        start: "top 10",             
-        end: "bottom top",            
-        pin: "#scrollButton3",       
-        pinSpacing: false             
-    });
-
-    ScrollTrigger.create({
-        trigger: "#sticky-section4",   
-        start: "top 10",             
-        end: "bottom top",            
-        pin: "#scrollButton4",       
-        pinSpacing: false             
-    });
         
     
     // // Accordion causes the ScrollTrigger to refresh
